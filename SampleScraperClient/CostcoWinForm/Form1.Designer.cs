cfdaace72b8944891f45f858652ec8940876f2b3
zjding <zjding@gmail.com>
2016-03-27 21:48:30 -0500

    3/27

--- SampleScraperClient/CostcoWinForm/Form1.Designer.cs
+++ SampleScraperClient/CostcoWinForm/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.btnProductInfo = new System.Windows.Forms.Button();
+            this.btnImportCategories = new System.Windows.Forms.Button();
+            this.btnImportProducts = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnProductInfo
             // 
-            this.btnProductInfo.Location = new System.Drawing.Point(110, 150);
+            this.btnProductInfo.Location = new System.Drawing.Point(96, 195);
             this.btnProductInfo.Name = "btnProductInfo";
             this.btnProductInfo.Size = new System.Drawing.Size(75, 23);
             this.btnProductInfo.TabIndex = 0;
@@ -41,11 +43,33 @@
             this.btnProductInfo.UseVisualStyleBackColor = true;
             this.btnProductInfo.Click += new System.EventHandler(this.btnProductInfo_Click);
             // 
+            // btnImportCategories
+            // 
+            this.btnImportCategories.Location = new System.Drawing.Point(32, 34);
+            this.btnImportCategories.Name = "btnImportCategories";
+            this.btnImportCategories.Size = new System.Drawing.Size(118, 23);
+            this.btnImportCategories.TabIndex = 1;
+            this.btnImportCategories.Text = "Import Categories";
+            this.btnImportCategories.UseVisualStyleBackColor = true;
+            this.btnImportCategories.Click += new System.EventHandler(this.btnImportCategories_Click);
+            // 
+            // btnImportProducts
+            // 
+            this.btnImportProducts.Location = new System.Drawing.Point(32, 63);
+            this.btnImportProducts.Name = "btnImportProducts";
+            this.btnImportProducts.Size = new System.Drawing.Size(118, 23);
+            this.btnImportProducts.TabIndex = 2;
+            this.btnImportProducts.Text = "Import Products";
+            this.btnImportProducts.UseVisualStyleBackColor = true;
+            this.btnImportProducts.Click += new System.EventHandler(this.btnImportProducts_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 399);
+            this.Controls.Add(this.btnImportProducts);
+            this.Controls.Add(this.btnImportCategories);
             this.Controls.Add(this.btnProductInfo);
             this.Name = "Form1";
             this.Text = "Main";
@@ -56,6 +80,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnProductInfo;
+        private System.Windows.Forms.Button btnImportCategories;
+        private System.Windows.Forms.Button btnImportProducts;
     }
 }
 
--- SampleScraperClient/CostcoWinForm/Form1.cs
+++ SampleScraperClient/CostcoWinForm/Form1.cs
@@ -8,10 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using ScrapySharp.Core;
+using ScrapySharp.Html.Parsing;
+using ScrapySharp.Network;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Html.Forms;
+using System.Data.SqlClient;
+
 namespace CostcoWinForm
 {
     public partial class Form1 : Form
     {
+        List<String> categoryUrlArray = new List<string>();
+        List<String> categoryArray = new List<string>();
+        List<String> subCategoryUrlArray = new List<string>();
+        List<String> productUrlArray = new List<string>();
+
+        ScrapingBrowser Browser = new ScrapingBrowser();
+        WebPage PageResult;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +38,278 @@ namespace CostcoWinForm
             ProductInfoForm productInfoForm = new ProductInfoForm();
             productInfoForm.ShowDialog();
 
+        }
+
+        private void btnImportCategories_Click(object sender, EventArgs e)
+        {
+            
+            Browser.AllowAutoRedirect = true; // Browser has many settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+            //go to the home page
+            PageResult = Browser.NavigateToPage(new Uri("http://www.costco.com/view-more.html"));
+
+            List<String> Names = new List<string>();
+            List<HtmlNode> columnNodes = PageResult.Html.CssSelect(".viewmore-column").ToList<HtmlNode>();
+
+            string connectionString = "Data Source=DESKTOP-ABEPKAT;Initial Catalog=Costco;Integrated Security=False;User ID=sa;Password=G4indigo;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string sqlString;
+
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+
+            cn.Open();
+
+            sqlString = "TRUNCATE TABLE Categories";
+            cmd.CommandText = sqlString;
+            cmd.ExecuteNonQuery();
+
+            foreach (HtmlNode columnNode in columnNodes)
+            {
+                string departmentName = (columnNode.SelectSingleNode("span")).InnerText;
+                departmentName = departmentName.Replace("'", "");
+
+                List<HtmlNode> ulNodes = columnNode.SelectNodes("ul").ToList<HtmlNode>();
+
+                List<HtmlNode> liNodes = ulNodes[0].SelectNodes("li").ToList<HtmlNode>();
+
+                foreach (var node1 in liNodes)
+                {
+                    string categoryName = node1.InnerText;
+                    categoryName = categoryName.Replace("'", "");
+
+                    if (node1.ChildNodes[0].Attributes[0].Name == "href")
+                    {
+                        sqlString = "INSERT INTO Categories (DepartmentName, CategoryName, CategoryUrl) VALUES ('" + 
+                                    departmentName + "', '" + 
+                                    categoryName + "', '" + 
+                                    node1.ChildNodes[0].Attributes[0].Value + "')";
+                        cmd.CommandText = sqlString;
+                        cmd.ExecuteNonQuery(); 
+                    }
+                }
+            }
+
+            MessageBox.Show("Get Category Done");
+
+            
+        }
+
+        private void btnImportProducts_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-ABEPKAT;Initial Catalog=Costco;Integrated Security=False;User ID=sa;Password=G4indigo;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string sqlString;
+
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+
+            cn.Open();
+            sqlString = "SELECT CategoryUrl FROM Categories WHERE bInclude = 1";
+            cmd.CommandText = sqlString;
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    categoryUrlArray.Add(reader.GetString(0));
+                }
+            }
+            reader.Close();
+
+            GetSubCategoryUrls();
+            MessageBox.Show("Get subCategoryUrlArray Done");
+
+            GetProductUrls();
+            MessageBox.Show("Get productUrlArray Done");
+
+            sqlString = "TRUNCATE TABLE Staging_ProductInfo";
+            cmd.CommandText = sqlString;
+            cmd.ExecuteNonQuery();
+
+            productUrlArray.Add("1");
+
+            foreach (string productUrl in productUrlArray)
+            {
+                try
+                {
+
+                    //string a = "http://www.costco.com/Bolero-Brown-Woven-100%25-Polypropylene-Rug-Collection-.product.100053980.html";
+
+                    PageResult = Browser.NavigateToPage(new Uri(productUrl));
+
+                    if (PageResult.Html.InnerText.Contains("Product Not Found"))
+                        continue;
+
+                    HtmlNode productInfo = PageResult.Html.CssSelect(".product-info").ToList<HtmlNode>().First();
+
+                    List<HtmlNode> topReviewPanelNode = productInfo.CssSelect(".top_review_panel").ToList<HtmlNode>();
+                    string productName = ((topReviewPanelNode[0]).SelectNodes("h1"))[0].InnerText;
+                    productName = productName.Replace("???", "");
+                    productName = productName.Replace("??", "");
+
+                    //if (productName.Contains("DESITIN") || productName.Contains("Evenflo"))
+                    //{
+                    //    int a = 1;
+                    //}
+
+                    List<HtmlNode> col1Node = productInfo.CssSelect(".col1").ToList<HtmlNode>();
+                    string itemNumber = (col1Node[0].SelectNodes("p")[0]).InnerText;
+                    if (itemNumber.Length > 6)
+                        itemNumber = itemNumber.Substring(6);
+                    else
+                        itemNumber = "";
+
+                    string price;
+                    List<HtmlNode> yourPriceNode = col1Node.CssSelect(".your-price").ToList<HtmlNode>();
+                    if (yourPriceNode.Count > 0)
+                    {
+                        List<HtmlNode> priceNode = yourPriceNode[0].CssSelect(".currency").ToList<HtmlNode>();
+                        price = priceNode[0].InnerText;
+                        price = price.Replace("$", "");
+                        price = price.Replace(",", "");
+
+                        if (price == "- -")
+                            price = "-2";
+                    }
+                    else
+                    {
+                        price = "-1";
+                    }
+
+                    var productOptionsNode = col1Node.CssSelect(".product-option").FirstOrDefault();
+
+                    string shipping = "0";
+
+                    var productSHNode = col1Node[0].SelectSingleNode("//li[@class='product']");
+
+                    if (productSHNode != null)
+                    {
+                        if (productSHNode.InnerText.ToUpper().Contains("INCLUDED"))
+                        {
+                            shipping = "0";
+                        }
+                        else
+                        {
+                            string shString = productSHNode.InnerText;
+                            int nDollar = shString.IndexOf("$");
+                            int nStar = shString.IndexOf("*");
+                            shString = shString.Substring(nDollar+1, nStar - nDollar - 1);
+                            shString = shString.Replace(" ", "");
+                            shipping = shString;
+                        }
+                    }
+
+                    
+                    //List<HtmlNode> productsNode = col1Node.CssSelect(".products").ToList<HtmlNode>();
+                    //if (productsNode.Count > 0)
+                    //    shipping = productsNode[0].SelectSingleNode("li").SelectSingleNode("p") == null ? "-1" : productsNode[0].SelectSingleNode("li").SelectSingleNode("p").InnerText;
+                    //else
+                    //    shipping = "-1";
+
+                    HtmlNode productDetailTabsNode = PageResult.Html.CssSelect(".product-detail-tabs").ToList<HtmlNode>().First();
+
+                    var productDetailTab1Node = productDetailTabsNode.SelectSingleNode("//div[@id='product-tab1']");
+
+                    var productDescriptionNode = productDetailTab1Node.SelectSingleNode("//p[@itemprop='description']");
+
+                    string description = productDescriptionNode.InnerHtml;
+                    description = description.Replace("???", "");
+                    description = description.Replace("'", "\"");
+
+                    var productDetailTab2Node = productDetailTabsNode.SelectSingleNode("//div[@id='product-tab2']");
+
+                    string specification = "";
+                    if (productDetailTab2Node != null)
+                    {
+                        specification = productDetailTab2Node.InnerHtml;
+                        specification = specification.Replace("'", "\"");
+                    }
+
+                    HtmlNode imageColumnNode = PageResult.Html.CssSelect(".image-column").ToList<HtmlNode>().First();
+
+                    //HtmlNode carouselWrapNode = imageColumnNode.CssSelect(".product-image-carousel").ToList<HtmlNode>().First();
+                    //var thumbHolderNode = carouselWrapNode.SelectNodes("//ul[@id='thumb_holder']");
+
+                    //var thumbNodes = thumbHolderNode.SelectNodes("/li");
+
+                    HtmlNode imageNode = imageColumnNode.SelectSingleNode("//img[@itemprop='image']");
+
+                    string imageUrl = (imageNode.Attributes["src"]).Value;
+
+                    sqlString = "INSERT INTO Staging_ProductInfo (Name, ItemNumber, Price, Shipping, Details, Specification, ImageLink, Url) VALUES ('" + productName + "','" + itemNumber + "'," + price + "," + shipping + "," + "'" + description + "','" + specification + "','" + imageUrl + "','" + productUrl + "')";
+                    cmd.CommandText = sqlString;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception exception)
+                {
+                    continue;
+                }
+            }
+
+            cn.Close();
+        }
+
+        private void GetSubCategoryUrls()
+        {
+            foreach (var categoryUrl in categoryUrlArray)
+            {
+                string url;
+                if (categoryUrl.Contains("http"))
+                    url = categoryUrl;
+                else
+                    url = "http://www.costco.com" + categoryUrl;
+                WebPage PageResult = Browser.NavigateToPage(new Uri(url));
+                var mainContentWrapperNote = PageResult.Html.SelectSingleNode("//div[@id='main_content_wrapper']");
+                if (mainContentWrapperNote == null)
+                    continue;
+                List<HtmlNode> categoryNodes = mainContentWrapperNote.CssSelect(".department_facets").ToList<HtmlNode>();
+
+                if (categoryNodes.CssSelect(".departmentContainer").Count() == 0)
+                {
+                    subCategoryUrlArray.Add(url);
+                }
+                else
+                {
+                    List<HtmlNode> departmentNodes = categoryNodes.CssSelect(".departmentContainer").ToList<HtmlNode>();
+                    foreach (HtmlNode departmentNode in departmentNodes)
+                    {
+                        HtmlNode node = departmentNode.Descendants("a").First();
+                        string departmentUrl = node.Attributes["href"].Value;
+                        subCategoryUrlArray.Add(departmentUrl);
+                    }
+                }
+            }
+        }
+
+        private void GetProductUrls()
+        {
+            foreach (string url in subCategoryUrlArray)
+            {
+                PageResult = Browser.NavigateToPage(new Uri(url));
+                var mainContentWrapperNote = PageResult.Html.SelectSingleNode("//div[@id='main_content_wrapper']");
+                List<HtmlNode> categoryNodes = mainContentWrapperNote.CssSelect(".department_facets").ToList<HtmlNode>();
+
+                if (categoryNodes.CssSelect(".departmentContainer").Count() == 0)
+                {
+                    List<HtmlNode> gridNodes = PageResult.Html.CssSelect(".grid-4col").ToList<HtmlNode>();
+                    if (gridNodes.Count() > 0)
+                    {
+                        List<HtmlNode> productNodes = gridNodes.CssSelect(".product-tile").ToList<HtmlNode>();
+
+                        foreach (HtmlNode productNode in productNodes)
+                        {
+                            HtmlNode product = productNode.CssSelect(".product-tile-image-container ").First();
+                            if (((product.SelectNodes("a")).First().Attributes).First().Name == "href")
+                            {
+                                productUrlArray.Add(((product.SelectNodes("a")).First().Attributes).First().Value);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
