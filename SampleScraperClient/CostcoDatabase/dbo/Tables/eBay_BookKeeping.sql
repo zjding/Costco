0ec9acfd0a7173fd047a03aa6e9670e3c083c32f
zjding <zjding@gmail.com>
2016-07-18 16:45:41 -0500

    7/18

---
+++ SampleScraperClient/CostcoDatabase/dbo/Tables/eBay_BookKeeping.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[eBay_BookKeeping] (
+    [TransactionDate] DATE           NULL,
+    [Amount]          DECIMAL (18)   NULL,
+    [Note]            NVARCHAR (200) NULL,
+    [Receipt]         NVARCHAR (500) NULL,
+    [Gain]            BIT            NULL
+);
+
